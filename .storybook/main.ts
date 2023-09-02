2e52b2d17f75c9ee335eff6dacf76902f96db5aa
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-09-02 09:54:34 +0300

    initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,35 @@
+import type {StorybookConfig} from "@storybook/nextjs";
+import * as path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+    ({
+          name: "@storybook/addon-styling",
+          options: {}
+        })
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal: (config, options) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@/*' : path.resolve(__dirname, '../src/*'),
+        '@/common/ui': path.resolve(__dirname, '../src/common/ui')
+      }
+    }
+
+    return config
+  }
+};
+export default config;
---
+++ src/common/ui/button/Button.tsx
@@ -0,0 +1,44 @@
+import React, { ComponentPropsWithRef, ReactNode } from 'react';
+
+import clsx from 'clsx';
+
+export type ButtonVariant = 'contained' | 'outlined';
+export type ButtonSize = 'sm' | 'md';
+
+export interface ButtonProps extends ComponentPropsWithRef<'button'> {
+  children: ReactNode;
+  loading?: boolean;
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+  variant?: ButtonVariant;
+  size?: ButtonSize;
+}
+
+export const Button = ({
+  children,
+  loading,
+  startIcon,
+  endIcon,
+  variant = 'contained',
+  size = 'sm',
+  ...props
+}: ButtonProps) => {
+  const className = 'content-center w-full rounded-full align-middle text-sm border-[1.5px]';
+  const containedClassName =
+    'border-primary bg-primary text-light font-semibold hover:bg-pr-hover border-pr-hover';
+  const outlinedClassName = 'border-secondary bg-light text-primary hover:border-primary bg-light';
+
+  const sizeDefaultClassName = 'py-[6px] px-[20px]';
+  const sizeMdClassname = 'py-[18px] px-[24px] min-h-[60px]';
+
+  const variantClassName = variant === 'contained' ? containedClassName : outlinedClassName;
+  const sizeClassName = size === 'sm' ? sizeDefaultClassName : sizeMdClassname;
+
+  return (
+    <button type='button' className={clsx(className, variantClassName, sizeClassName)} {...props}>
+      {startIcon && startIcon}
+      {children}
+      {endIcon && endIcon}
+    </button>
+  );
+};
