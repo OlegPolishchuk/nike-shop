ca346e335dc79a9b1b144147e8f42c46aae0ff29
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-09-30 20:16:29 +0300

    fix: getServerSide redirect from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,9 @@ import React from 'react';
 import { GetServerSideProps } from 'next';
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const BaseUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL;
-
   return {
     redirect: {
-      destination: BaseUrl + '/ru',
+      destination: '/ru',
       permanent: true,
     },
     props: {},
