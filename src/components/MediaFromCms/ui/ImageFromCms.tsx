6749e0bc77e3a660a5a3719d653e8722156a9503
OlegPolishchuk <oleg.makkorm@gmail.com>
2023-11-12 16:41:00 +0300

    feat: add image placeholder

--- src/components/MediaFromCms/ui/ImageFromCms.tsx
+++ src/components/MediaFromCms/ui/ImageFromCms.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ImageProps } from 'next/dist/shared/lib/get-img-props';
 import Image from 'next/image';
@@ -6,10 +6,13 @@ import Image from 'next/image';
 const BaseUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL || '';
 
 export const ImageFromCms = ({ src, alt, className, ...restProps }: ImageProps) => {
+  const [imgSrc, setSrc] = useState(BaseUrl + src);
+
   return (
     <Image
-      src={BaseUrl + src}
+      src={imgSrc}
       alt={alt}
+      onError={() => setSrc('../../../../public/images/product-placeholder-wp.jpg')}
       {...restProps}
       priority
       sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
